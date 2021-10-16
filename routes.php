ae114458fe8a44b62a2bb30a647834ab183bbb62
nutnareexx <nutnaree.co@ku.th>
2021-10-16 15:00:54 +0700

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,29 @@
+<?php 
+$controllers = array('pages'=>['home','error'],'medicine'=>['index']);
+
+function call($controller,$action){
+    require_once("controllers/".$controller."_controller.php");
+    switch($controller)
+    {
+        case"pages": $controller = new PagesController();
+        break;
+        case"medicine": $controller = new MedicineController();
+        break;
+    }
+    $controller->{$action}();
+}
+
+if(array_key_exists($controller,$controllers))
+{
+    if(in_array($action,$controllers[$controller]))
+    {
+        call($controller,$action);
+    }
+    else{
+        call('pages','error');
+    }
+}
+else{
+    call('pages','error');
+}
+?>
