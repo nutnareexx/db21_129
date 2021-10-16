398353a673a5658b5908a6614d783f01d6e33c2f
nutnareexx <nutnaree.co@ku.th>
2021-10-16 15:01:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET['controller']) && isset($_GET['action']))
+{
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+}
+else{
+    $controller = 'pages';
+    $action = 'home';
+}
+?>
+
+<html>
+<head></head>
+<body>
+    <?php echo "controller = ".$controller.", action = ".$action ;?>
+    <br>[<a href=""> HOME </a>]
+    <br>[<a href="?controller=medicine&action=index"> medicine </a>]
+    <?php require_once("routes.php");?>
+</body>
+</html>
