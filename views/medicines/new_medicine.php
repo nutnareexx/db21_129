fe45d42eb3dc55226ce621971bf8023cfbfb0def
nutnareexx <nutnaree.co@ku.th>
2021-10-17 23:30:09 +0700

    Create new_medicine.php

---
+++ views/medicines/new_medicine.php
@@ -0,0 +1,15 @@
+<br>
+<form method="get" action="">
+<br><label> Medicine ID <input type="text" name="id"/> </label><br>
+<br><label> Medicine NAME <input type="text" name="name"/> </label><br>
+<br><label> Medicine DETAIL <input type="text" name="detail"/> </label><br>
+<br><label> Medicine EFFECT <input type="text" name="effect"/> </label><br>
+<br><label> Medicine TIME <input type="text" name="time"/> </label><br>
+<br><label> Medicine DOSE <input type="text" name="dose"/> </label><br>
+<br><label> Medicine DURATION <input type="text" name="duration"/> </label><br>
+<br>
+
+<input type="hidden" name="controller" value="medicine"/>
+<button type="submit" name="action" value="index"> BACK </button>
+<button type="submit" name="action" value="addMedicine"> SAVE </button>
+</form>
