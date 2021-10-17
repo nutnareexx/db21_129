e9873165f2aeb51338616374b46f7f4e02c6ecf0
nutnareexx <nutnaree.co@ku.th>
2021-10-17 23:29:44 +0700

    Update medicine_controller.php

--- controllers/medicine_controller.php
+++ controllers/medicine_controller.php
@@ -3,8 +3,33 @@ class MedicineController
 {
     public function index()
     {
-        $medicine_list = Medicine::getAll();
+        $medicineList = Medicine::getAll();
         require_once('views/medicines/index_medicine.php');
+    }
+    
+    public function search()
+    {
+        $key = $_GET['key'];
+        $medicineList = Medicine::search($key);
+        require_once('views/medicines/index_medicine.php');
+    }
+
+    public function newMedicine()
+    {
+        require_once('views/medicines/new_medicine.php');
+    }
+
+    public function addMedicine()
+    {
+        $id = $_GET['id'];
+        $name = $_GET['name'];
+        $detail = $_GET['detail'];
+        $effect = $_GET['effect'];
+        $dose = $_GET['dose'];
+        $time = $_GET['time'];
+        $duration = $_GET['duration'];
+        Medicine::add($id,$name,$detail,$effect,$dose,$time,$duration);
+        MedicineController::index();
     }
    
 }
