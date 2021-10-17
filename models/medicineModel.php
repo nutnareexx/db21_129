74ddaea000d806501cb4eec35ddd933c3bd34f7b
nutnareexx <nutnaree.co@ku.th>
2021-10-17 23:29:57 +0700

    Update medicineModel.php

--- models/medicineModel.php
+++ models/medicineModel.php
@@ -9,36 +9,72 @@ class Medicine{
     public $duration;
 
 
-public function __construct($id,$name,$detail,$effect,$dose,$time,$duration)
-{
-    $this->id = $id;
-    $this->name = $name;
-    $this->detail = $detail;
-    $this->effect = $effect;
-    $this->dose = $dose;
-    $this->time = $time;
-    $this->duration = $duration;
-}
 
-public static function getAll()
-{
-    $medicineList=[];
-    require("connection_connect.php");
-    $sql = "SELECT * FROM `medicine`";
-    $result = $conn->query($sql);
-    while($my_row = $result->fetch_assoc())
+
+    public function __construct($id,$name,$detail,$effect,$dose,$time,$duration)
     {
-        $id=$my_row[Mid];
-        $name=$my_row[Mname];
-        $detail=$my_row[Mdetail];
-        $effect=$my_row[Meffect];
-        $dose=$my_row[Mdose];
-        $time=$my_row[Mtime];
-        $duration=$my_row[Mduration];
+        $this->id = $id;
+        $this->name = $name;
+        $this->detail = $detail;
+        $this->effect = $effect;
+        $this->dose = $dose;
+        $this->time = $time;
+        $this->duration = $duration;
+    }
+
+    public static function getAll()
+    {
+        $medicineList=[];
+        require("connection_connect.php");
+        $sql = "SELECT * FROM `medicine`";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $id=$my_row[Mid];
+            $name=$my_row[Mname];
+            $detail=$my_row[Mdetail];
+            $effect=$my_row[Meffect];
+            $dose=$my_row[Mdose];
+            $time=$my_row[Mtime];
+            $duration=$my_row[Mduration];
+            $medicineList[] = new Medicine($id,$name,$detail,$effect,$dose,$time,$duration);
+        }
+        require("connection_close.php");
+        return $medicineList;
+    }
+
+    public static function search($key){
+        $medicineList=[];
+        require("connection_connect.php");
+        $sql = "SELECT * FROM `medicine` 
+        WHERE (Mid LIKE '%$key%' OR Mname LIKE '%$key%' OR Mdetail LIKE '%$key%' OR Meffect LIKE '%$key%' OR Mdose LIKE '%$key%' 
+        OR Mtime LIKE '%$key%' OR Mduration LIKE '%$key%')";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $id=$my_row[Mid];
+            $name=$my_row[Mname];
+            $detail=$my_row[Mdetail];
+            $effect=$my_row[Meffect];
+            $dose=$my_row[Mdose];
+            $time=$my_row[Mtime];
+            $duration=$my_row[Mduration];
+            $medicineList[] = new Medicine($id,$name,$detail,$effect,$dose,$time,$duration);
+        }
+        require("connection_close.php");
+        return $medicineList;
 
     }
-    require("connection_close.php");
-    return $medicineList;
-}
+
+    public static function add($id,$name,$detail,$effect,$dose,$time,$duration)
+    {
+        require("connection_connect.php");
+        $sql = "INSERT INTO `medicine`(`Mid`, `Mname`, `Mdetail`, `Meffect`, `Mdose`, `Mtime`, `Mduration`) 
+        VALUES ('$id','$name','$detail','$effect',$dose,'$time','$duration')";
+        $result = $conn->query($sql);
+        require("connection_close.php");
+        return "[ add success $result rows ]";
+    }
+
 }
 ?>
