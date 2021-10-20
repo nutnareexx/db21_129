f35ba7d3ab74f8da21744cf02fd6f793bb1abeff
nutnareexx <nutnaree.co@ku.th>
2021-10-21 01:32:08 +0700

    Update medicine_controller.php

--- controllers/medicine_controller.php
+++ controllers/medicine_controller.php
@@ -31,6 +31,41 @@ class MedicineController
         Medicine::add($id,$name,$detail,$effect,$dose,$time,$duration);
         MedicineController::index();
     }
+
+    public function updateForm()
+    {
+        
+        $id=$_GET['id'];
+        $medList = Medicine::get($id);
+        require_once('views/medicines/update_medicine.php');
+    }
+
+    public function update()
+    {
+        $id = $_GET['id'];
+        $name = $_GET['name'];
+        $detail = $_GET['detail'];
+        $effect = $_GET['effect'];
+        $dose = $_GET['dose'];
+        $time = $_GET['time'];
+        $duration = $_GET['duration'];
+        Medicine::update($id,$name,$detail,$effect,$dose,$time,$duration);
+        MedicineController::index();
+    }
+
+    public function deleteConfirm()
+    {
+        $id=$_GET['id'];
+        $medList = Medicine::get($id);
+        require_once('views/medicines/delete_medicine.php');
+    }
+
+    public function delete()
+    {
+        $id = $_GET['x'];
+        Medicine::delete($id);
+        MedicineController::index();
+    }
    
 }
 ?>
