ba606a96ec52ae02434fa0536c105eb162edc2b1
nutnareexx <nutnaree.co@ku.th>
2021-11-07 17:53:01 +0700

    Create doctorModel.php

---
+++ models/doctorModel.php
@@ -0,0 +1,39 @@
+<?php
+class Doctor{
+    public $Docid;
+    public $Docname;
+    public $Doclast;
+    public $Docphon;
+    public $Dochosid;
+
+
+    public function __construct($docid,$docname,$doclast,$docphon,$dochospi)
+    {
+        $this->Docid = $docid;
+        $this->Docname = $docname;
+        $this->Doclast = $doclast;
+        $this->Docphon = $docphon;
+        $this->Dochosid =$dochospi;
+        
+    }
+
+    public static function getAll()
+    {
+        $doctorList=[];
+        require("connection_connect.php");
+        $sql = "SELECT * FROM `Doctor`";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $Docid=$my_row[doctor_id];
+            $Docname=$my_row[name_D];
+            $Doclast=$my_row[lastname_D];
+            $Docphon=$my_row[phone_D];
+            $Dochosid=$my_row[hospital_id];
+            $doctorList[] = new Doctor($Docid,$Docname,$Doclast,$Docphon,$Dochosid);
+        }
+        require("connection_close.php");
+        return $doctorList;
+    }
+}
+?>
