12eebee035d5fa28d01deadf89fdcc42e90f76fb
nutnareexx <nutnaree.co@ku.th>
2021-11-07 17:52:26 +0700

    Create newPrescription.php

---
+++ views/prescriptions/newPrescription.php
@@ -0,0 +1,26 @@
+<br>
+<form method="get" action="">
+<br><label> Prescription ID <input type="text" name="preid"/> </label>
+<br>
+<br><label> Telemedicine ID <select name="teleid">
+    <?php foreach($telemedicineList as $t){
+        echo "<option value=$t->tid> $t->tid </option>";
+    }
+    ?> 
+    </select></label>
+<br>
+<br><label> Prescription Date <input type="date" name="predate"/> </label>
+<br>
+<br><label> Doctor ID <select name="docID">
+    <?php foreach($doctorList as $d){
+        echo "<option value=$d->Docid> $d->Docid </option>";
+    }
+    ?> 
+    </select></label>
+<br>
+<br>
+
+<input type="hidden" name="controller" value="prescription"/>
+<button type="submit" name="action" value="index"> BACK </button>
+<button type="submit" name="action" value="addPrescription"> NEXT </button>
+</form>
