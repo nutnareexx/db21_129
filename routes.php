588ce71a68b9dabb794fc4ad1975e0e2ec43968b
nutnareexx <nutnaree.co@ku.th>
2021-11-07 17:52:36 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,15 +1,28 @@
 <?php 
 $controllers = array('pages'=>['home','error'],
-                    'medicine'=>['index','newMedicine','addMedicine','search','updateForm','update','deleteConfirm','delete']);
+                    'medicine'=>['index','newMedicine','addMedicine','search','updateForm','update','deleteConfirm','delete'],
+                    'prescription'=>['index','newPrescription','newdeatilPrescription','addPrescription','add_detailPrescription','search','updateForm','update','deleteConfirm','delete']);
 
 function call($controller,$action){
     require_once("controllers/".$controller."_controller.php");
     switch($controller)
     {
-        case"pages": $controller = new PagesController();
+        case"pages": 
+            $controller = new PagesController();
         break;
-        case"medicine": require_once("models/medicineModel.php");
+
+        case"medicine": 
+            require_once("models/medicineModel.php");
             $controller = new MedicineController();
+        break;
+        
+        case"prescription": 
+            require_once("models/prescriptionModel.php");
+            require_once("models/medicineModel.php");
+            require_once("models/doctorModel.php");
+            require_once("models/telemedicineModel.php");
+            require_once("models/pre_detail.php");
+            $controller = new PreprescriptionController();
         break;
     }
     $controller->{$action}();
