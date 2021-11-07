1199c034351dda4ac57546086462f5aed2038488
nutnareexx <nutnaree.co@ku.th>
2021-11-07 17:52:33 +0700

    Create delete_prescription.php

---
+++ views/prescriptions/delete_prescription.php
@@ -0,0 +1,22 @@
+<?php echo "<br> Are you sure to delete this medicine <br>
+            <br> Prescription ID : $pList->pid
+            <br> Telemedicine ID : $pList->teleID
+            <br> Prescription Date : $pList->preDate
+            <br> Doctor ID : $pList->d_ID
+            <br> Doctor Name : $pList->dname $pList->dlast
+            <br> Medicine ID : $pList->mid
+            <br> Medicine Name : $pList->mname
+            <br> Prescription Dose : $pList->predose
+            <br> Prescription Note : $pList->preNote 
+            <br>
+            <br>";
+
+?>
+
+<form method="get" action="">
+    <input type="hidden" name="controller" value="prescription"/>
+    <input type="hidden" name="x" value="<?php echo $pList->pid;?>"/>
+    <input type="hidden" name="y" value="<?php echo $pList->mid;?>"/>
+    <button type="submit" name="action" value="index"> BACK </button>
+    <button type="submit" name="action" value="delete"> DETELE </button>
+</form>
