264e9cfbb3fb5c40e3a3486902041f8467028874
nutnareexx <nutnaree.co@ku.th>
2021-11-07 17:52:38 +0700

    Create telemedicineModel.php

---
+++ models/telemedicineModel.php
@@ -0,0 +1,39 @@
+<?php
+class Telemedicine{
+    public $tid;
+    public $pid;
+    public $tsym;
+    public $ttem;
+    public $tdate;
+
+
+    public function __construct($tid,$pid,$tsym,$ttem,$tdate)
+    {
+        $this->tid = $tid;
+        $this->pid = $pid;
+        $this->tsym = $tsym;
+        $this->ttem = $ttem;
+        $this->tdate =$tdate;
+        
+    }
+
+    public static function getAll()
+    {
+        $telemedicineList=[];
+        require("connection_connect.php");
+        $sql = "SELECT * FROM `telemedicine`";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $tid=$my_row[teleID];
+            $pid=$my_row[patient_id];
+            $tsym=$my_row[symptom];
+            $ttem=$my_row[temperature];
+            $tdate=$my_row[teledate];
+            $telemedicineList[] = new Telemedicine($tid,$pid,$tsym,$ttem,$tdate);
+        }
+        require("connection_close.php");
+        return $telemedicineList;
+    }
+}
+?>
