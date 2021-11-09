b3eb808a628993c2c41a7a390e5e05ad74a99cf4
nutnareexx <nutnaree.co@ku.th>
2021-11-09 13:20:18 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -2,3 +2,17 @@
 
 
 
+
+<table border="1">
+    <tr> <td> Title </td> <td> Quantity </td>  </tr>
+
+    <?php foreach($hList as $h)
+    {
+        echo "<tr> <td> $h->t </td> 
+        <td>  $h->c </td> 
+    
+        </tr>";
+    }
+    echo"</table>";
+    ?>
+
