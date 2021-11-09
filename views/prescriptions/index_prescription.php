b3915228dbe5eef08b3d7c54d8360f446b10c7d5
nutnareexx <nutnaree.co@ku.th>
2021-11-09 08:40:25 +0700

    Update index_prescription.php

--- views/prescriptions/index_prescription.php
+++ views/prescriptions/index_prescription.php
@@ -12,14 +12,17 @@
 
 <table border="1">
     <tr> <td> Prescription ID </td> <td> Telemedicine ID </td> <td> Date </td> 
-    <td> Doctor ID </td> <td> Doctor Name </td> <td> Doctor Lastname </td>
+    <td> Doctor ID </td> <td> Doctor Name </td>
     <td> Medicine ID </td> <td> Medicine Name </td> <td> Dose </td> <td> Note </td>
      <td> UPDATE </td> <td> DELETE </td> </tr>
 
     <?php foreach($prescriptionList as $p)
     {
-        echo "<tr> <td> $p->pid </td> <td> $p->teleID </td> <td> $p->preDate </td> 
-        <td> $p->d_ID </td> <td> $p->dname </td> <td> $p->dlast </td> 
+        echo "<tr> <td> $p->pid </td> 
+        <td> <a href= http://158.108.207.4/db21/db21_127/?controller=telemedicine&action=search&key=$p->teleID /> $p->teleID </a> </td> 
+        <td> $p->preDate </td> 
+        <td> <a href= http://158.108.207.4/db21/db21_125/Myweb/?controller=doctor&action=search&key=$p->d_ID  /> $p->d_ID </a> </td> 
+        <td> $p->dname 
         <td> <a href=?controller=medicine&action=search&key=$p->mid> $p->mid </td> 
         <td> $p->mname </td> <td> $p->predose </td> <td> $p->preNote </td> 
 
